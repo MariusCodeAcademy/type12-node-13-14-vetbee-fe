70cbbdc4d382065daf4c44179c90e2c36ab34ab6
Marius <marius@example.com>
2022-09-09 10:28:03 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Card from './modules/class/Card.js';
+import List from './modules/class/List.js';
+import { PetsService } from './modules/class/Http.js';
+
+console.log('main.js');
+
+const listEl = document.getElementById('pets-list');
+
+const flow = async () => {};
+
+flow();
---
+++ js/modules/class/Http.js
@@ -0,0 +1,25 @@
+export default class Http {
+  static baseUrl = 'http://localhost:3000/api/v1';
+
+  static async get(url) {
+    try {
+      const resp = await fetch(`${Http.baseUrl}${url}`);
+      if (!resp.ok)
+        throw new Error(
+          `fetch fail: ${resp.status} ${resp.statusText} trying to fetch ${resp.url}`
+        );
+      const dataInJs = await resp.json();
+      console.log('http:', dataInJs);
+      return dataInJs;
+    } catch (error) {
+      console.warn('klaida http ', error.message);
+      console.warn('error.stack ', error.stack);
+    }
+  }
+}
+
+export class PetsService extends Http {
+  static getAll() {
+    return PetsService.get('/pets');
+  }
+}
